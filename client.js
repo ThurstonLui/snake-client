dc7ddc828ea76b21534372e47687a4bc5a4205a7
Thurston Lui <thurstonlui@gmail.com>
2020-09-23 21:58:36 +0000

    get snake to move via setInterval

--- client.js
+++ client.js
@@ -7,9 +7,16 @@ const connect = function() {
   const conn = net.createConnection({ 
     host: '135.23.222.131',
     port: 50542
-
-    //Event handler to handle incoming data and console log it
   });
+    //Event handler to handle incoming data and console log it
+  // }, () => {
+  //   console.log("Successfully connected to game server")
+  //   conn.write('Name: TPL');
+  //   conn.write("Move: up");
+  //   conn.write("Move: up");
+  //   conn.write("Move: up");
+  //   conn.write("Move: up");
+  // });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   
@@ -17,9 +24,21 @@ const connect = function() {
     console.log('Incomming:', data);
   }); 
 
+  
+
   conn.on('connect', () => {
     console.log("Successfully connected to game server")
-    conn.write('TPL: Hey');
+    conn.write('Name: TPL');
+    
+
+   
+   
+   
+    setInterval(() => { 
+      
+      conn.write("Move: up"); }, 50 )
+    
+   
   } );
 
 
